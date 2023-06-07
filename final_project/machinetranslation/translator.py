64393f4f15c6d2d74dedc2bf26ded583425f2f07
Hassan EL BAHI <me_80_me@hotmail.com>
2023-06-07 12:34:45 +0100

    pushing

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,46 @@
+"""AI is creating summary for Translation between EN:FR
+
+Returns:
+    [Text]: [description]
+"""
+import googletrans
+
+
+def english_to_french(text):
+    """AI is creating summary for english_to_french
+
+    Args:
+        text ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    # write the code here
+    translator = googletrans.Translator(src='en', dst='fr')
+    translation = translator.translate(text)
+    return translation
+
+
+def french_to_english(text):
+    """AI is creating summary for french_to_english
+
+    Args:
+        text ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    # write the code here
+    translator = googletrans.Translator(src='fr', dst='en')
+    translation = translator.translate(text)
+    return translation
+
+
+FR = 'Bonjour'
+EN = 'Hello'
+
+result_fr = english_to_french(EN)
+result_en = french_to_english(FR)
+
+print(result_fr)  # Output: 'Bonjour !'
+print(result_en)  # Output: 'Hello!'
